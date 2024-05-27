3f14efc66a23bbb8e2f2d988289390d463a7cab1
MiguelEleuterio <mike.eleuterio@hotmail.com>
2024-05-27 20:01:04 +0100

    Load medicine as user scrolls

--- MyApplication/app/src/main/java/pt/ulisboa/tecnico/cmov/myapplication/MedicineActivity.kt
+++ MyApplication/app/src/main/java/pt/ulisboa/tecnico/cmov/myapplication/MedicineActivity.kt
@@ -2,6 +2,7 @@ package pt.ulisboa.tecnico.cmov.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,9 @@ import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import pt.ulisboa.tecnico.cmov.myapplication.databinding.ActivityMedicineBinding
@@ -17,16 +20,28 @@ import java.util.Locale
 
 class MedicineActivity : AppCompatActivity() {
 
-    private lateinit var db: FirebaseFirestore
+    companion object {
+        val TAG = "MedicineActivity"
+    }
+
+    private var db: FirebaseFirestore = Firebase.firestore
 
     private lateinit var binding: ActivityMedicineBinding
-    private lateinit var medicineList: ArrayList<MedicineMetaData>
     private lateinit var adapter: MedicineAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView: SearchView
+    private var medicineList: ArrayList<MedicineMetaData> = ArrayList()
+    private var filteredList: ArrayList<MedicineMetaData> = ArrayList()
+    private var lastFetch: DocumentSnapshot? = null
+    private var isLoading: Boolean = false
+    private var hasMoreData: Boolean = true
+    private var inSearch: Boolean = false
+    private var prevQuerySize: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate started")
+
         //binding = ActivityMedicineBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_medicine)
         //setContentView(binding.root)
@@ -43,10 +58,9 @@ class MedicineActivity : AppCompatActivity() {
         // loading pop-up
         //displayProgressPopUp()
 
-        medicineList = ArrayList()
-        adapter = MedicineAdapter(this@MedicineActivity, medicineList)
+        adapter = MedicineAdapter(this@MedicineActivity, filteredList)
         //binding.medicineRecyclerView.adapter = adapter
-        //recyclerView.adapter = adapter
+        recyclerView.adapter = adapter
 
         adapter.onItemClick = {
             val intent = Intent(this, MedicineInformationPanelActivity::class.java)
@@ -54,44 +68,121 @@ class MedicineActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        eventChangeListener()
-
         searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                searchMedicine(newText)
+                newText?.let {
+                    if (it.length > prevQuerySize) {
+                        // User added text
+                        searchMedicine(newText, true)
+                    } else if (it.length < prevQuerySize) {
+                        // User deleted text
+                        // This assumes the user typed the query one by one and thus
+                        // substrings were already queried. If the user wrote it
+                        // in one go, e.g. by pasting text, this will miss some results
+                        searchMedicine(newText, false)
+                    }
+                    prevQuerySize = it.length
+                }
                 return true
             }
 
         })
 
-    }
-
-    private fun searchMedicine(query: String?) {
-        if (query != null) {
-            val searchList = ArrayList<MedicineMetaData>()
-            for (medicine in medicineList) {
-                if (medicine.name?.lowercase()?.contains(query.lowercase(Locale.getDefault())) == true) {
-                    searchList.add(medicine)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(1) && !isLoading && !inSearch && hasMoreData) {
+                    loadData()
                 }
             }
+        })
 
-            if (searchList.isEmpty()) {
-                Toast.makeText(this, "No medicine found", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                adapter.setMedicineList(searchList)
-                recyclerView.adapter = adapter
-            }
-        }
+        loadData()
+
+        Log.d(TAG, "onCreate finished")
     }
 
-    override fun onResume() {
-        super.onResume()
-        createBottomNavigation()
+    private fun loadData() {
+        Log.d(TAG, "Loading data from DB")
+        isLoading = true
+        var query: Query
+
+        query = db.collection("medicines").limit(10).orderBy("name")
+
+        lastFetch?.let {
+            query = query.startAfter(it)
+        }
+
+        query.get().addOnSuccessListener { documents ->
+            val initialSize = filteredList.size
+            for (document in documents) {
+                val item = document.toObject(MedicineMetaData::class.java)
+                if (!medicineList.contains(item)) {
+                    medicineList.add(item)
+                }
+                if (!filteredList.contains(item)) {
+                    filteredList.add(item)
+                }
+            }
+            if (!documents.isEmpty) {
+                lastFetch = documents.documents[documents.size() - 1]
+            }
+            if (initialSize == filteredList.size) {
+                hasMoreData = false
+            } else {
+                Log.d(TAG, "Notifying change from $initialSize to ${filteredList.size}")
+                adapter.notifyItemRangeChanged(initialSize, filteredList.size)
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Error loading data from DB", Toast.LENGTH_SHORT).show()
+        }
+
+        isLoading = false
+        Log.d(TAG, "Finished loading data from DB, medicineList=$medicineList, filteredList=$filteredList")
+    }
+
+    private fun searchMedicine(query: String?, new: Boolean) {
+        filteredList.clear()
+        if (query != null) {
+            inSearch = true
+
+            medicineList.filterTo(filteredList) {
+                it.name?.lowercase()?.contains(query.lowercase(Locale.getDefault())) == true
+            }
+
+            if (query.length >= 2 && new && hasMoreData) {
+                db.collection("medicines")
+                    .orderBy("name")
+                    .startAt(query)
+                    .endAt(query + "\uf8ff")
+                    .get()
+                    .addOnSuccessListener { documents ->
+                            for (document in documents) {
+                                val medicine = document.toObject(MedicineMetaData::class.java)
+                                if (!filteredList.contains(medicine)) {
+                                    filteredList.add(medicine)
+                                    if (!medicineList.contains(medicine)) {
+                                        medicineList.add(medicine)
+                                    }
+                                }
+                            }
+                        }.
+                        addOnFailureListener {
+                            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                        }
+            }
+            if (filteredList.isEmpty() && new) {
+                Toast.makeText(this, "No medicine found", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            inSearch = false
+            filteredList.addAll(medicineList)
+        }
+        adapter.notifyDataSetChanged()
     }
 
     private fun eventChangeListener() {
@@ -131,7 +222,7 @@ class MedicineActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.nav_medicine -> true
                 R.id.nav_map -> {
-                    //startActivity(Intent(applicationContext, MapsActivity::class.java))
+                    startActivity(Intent(applicationContext, MapsActivity::class.java))
                     finish()
                     true
                 }
--- MyApplication/app/src/main/java/pt/ulisboa/tecnico/cmov/myapplication/MedicineAdapter.kt
+++ MyApplication/app/src/main/java/pt/ulisboa/tecnico/cmov/myapplication/MedicineAdapter.kt
@@ -1,6 +1,9 @@
 package pt.ulisboa.tecnico.cmov.myapplication
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +12,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class MedicineAdapter(private val context: Context, private var dataList: List<MedicineMetaData>)
+class MedicineAdapter(private val context: Context, private var dataList: ArrayList<MedicineMetaData>)
     : RecyclerView.Adapter<MedicineAdapter.MedicineViewHolder>() {
     var onItemClick : ((MedicineMetaData) -> Unit)? = null
 
@@ -29,11 +32,22 @@ class MedicineAdapter(private val context: Context, private var dataList: List<M
 
     override fun onBindViewHolder(holder: MedicineViewHolder, position: Int) {
         val medicine = dataList[position]
-        Glide.with(context).load(medicine.image).into(holder.recImage)
+
+        if (checkConnectivity(context) == 2) {
+            Glide.with(context).load(medicine.image).into(holder.recImage)
+        } else {
+            Glide.with(context).load(R.drawable.placeholder).into(holder.recImage)
+        }
         holder.recName.text = medicine.name?.capitalizeFirstLetter()
 
         holder.itemView.setOnClickListener {
             onItemClick?.invoke(medicine)
+        }
+
+        holder.recImage.setOnClickListener {
+            if (checkConnectivity(context) != 0) {
+                Glide.with(context).load(medicine.image).into(holder.recImage)
+            }
         }
     }
 
@@ -43,11 +57,34 @@ class MedicineAdapter(private val context: Context, private var dataList: List<M
         }
     }
 
-    fun setMedicineList(searchList: List<MedicineMetaData>) {
+    fun setMedicineList(searchList: ArrayList<MedicineMetaData>) {
         dataList = searchList
-        notifyDataSetChanged() // TODO: nao ta a funcionar, o eventChangeListener nao listen este notify
     }
 
+    /**
+     * Checks what type of connectivity the user has
+     *
+     * @param context The context of the caller
+     * @return Int: 0 -> No connectivity; 1 -> Mobile Data; WiFi or stronger -> 2
+     */
+    private fun checkConnectivity(context: Context): Int {
+        //
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return 1
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return 2
+            }
+        }
+        return 0
+    }
 }
 
 
